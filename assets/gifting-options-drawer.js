5ab006c28ca0daf57164b3be4eb0cbe4aba243c2
jaredricardo <jaredricardo94@gmail.com>
2025-10-17 17:01:24 -0400

    gifting options state work

--- assets/gifting-options-drawer.js
+++ assets/gifting-options-drawer.js
@@ -26,7 +26,32 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    class HammittGiftingBrokenOutLineItem extends HTMLElement {
+        constructor() {
+            super()
+            this.querySelector('#no-box').addEventListener('change', this.handleLineItemBoxChange)
+            this.querySelector('#line-item-hand-written-note').addEventListener('change', this.handleLineItemGiftNoteChange)
+        }
+
+        handleLineItemBoxChange() {
+            const optOutOfBox = this.checked
+            const giftNoteCheckbox = this.closest('.broken-out-line-item').querySelector('#line-item-hand-written-note')
+            if(optOutOfBox) {
+                giftNoteCheckbox.checked = false
+            }
+        }
+
+        handleLineItemGiftNoteChange() {
+            const giftNoteChecked = this.checked
+            const boxOptOut = this.closest('.broken-out-line-item').querySelector('#no-box')
+            if(giftNoteChecked) {
+                boxOptOut.checked = false
+            }
+        }
+
+    }
     customElements.define('hammitt-gifting-options-drawer-trigger', HammittGiftingOptionsDrawerTrigger)
     customElements.define('hammitt-gifting-options-drawer', HammittGiftingOptionsDrawer)
+    customElements.define('hammitt-gifting-broken-out-line-item', HammittGiftingBrokenOutLineItem)
 
 })
