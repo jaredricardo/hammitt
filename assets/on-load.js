604dd294340bec11dedb499b109c77e2493f7ee5
jaredricardo <jaredricardo94@gmail.com>
2025-11-11 02:39:38 +0900

    on load deletion of _rv cookie for past users who may have gotten it

--- assets/on-load.js
+++ assets/on-load.js
@@ -1,6 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     window.buildCompleteTheSetInCart()
     window.setRecentlyViewedNav(false)
+
+    // temprorary clearing of the _rv cookie to avoid 400 NGINX error for users in the past
+    // who had viewed the right combination of products that caused the issue
+    window.document.cookie = '_rv=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+
 })
 
 window.addEventListener('load', () => {
